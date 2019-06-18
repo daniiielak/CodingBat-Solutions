6d5d1fb5bc8608b12c36c92a76b088926d861513
Daniela Kepper <danielaelisabeth.kepper@gmail.com>
2019-06-19 00:06:28 +0100

    finished String-3.java

--- Java/String-3.java
+++ Java/String-3.java
@@ -213,7 +213,22 @@ public class String-3 {
 
   // notReplace -> Given a string, return a string where every appearance of the lowercase word "is" has been replaced with "is not". The word "is" should not be immediately preceeded or followed by a letter -- so for example the "is" in "this" does not count. (Note: Character.isLetter(char) tests if a char is a letter.)
   // eg. 1. notReplace("is test") → "is not test" || 2. notReplace("is-is") → "is not-is not" || 3. notReplace("This is right") → "This is not right"
-  
+  public String notReplace(String str) {
+    String newWord = "";
+    for(int i = 0; i<str.length(); i++){
+      if((i-1>=0 && Character.isLetter(str.charAt(i-1))) || (i+2<str.length() && Character.isLetter(str.charAt(i+2)))){
+        newWord = newWord + str.charAt(i);
+      }
+      else if(i+1<str.length() && str.substring(i, i+2).equals("is")){
+        newWord = newWord + "is not";
+        i++;
+      }
+      else{
+        newWord = newWord + str.charAt(i);
+      }
+    }
+    return newWord;
+  }
 
   // DNC - class closing
 }
